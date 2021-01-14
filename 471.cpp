ce26cb54788b3aa7fb796598dc0b8c5d9032bb7d
AndoroidX <andriytretyakov@gmail.com>
2021-01-14 22:53:28 +0200

    Added new solutions
    469, 471, 475, 478

---
+++ 471.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+#define mod                 %1000000007
+#define FOR(i,e)            for(int i=0;i<e;i++)
+#define FORE(i,t,e)         for(int i=t;i<e;i++)
+#define pb                  push_back
+#define clr(x)              x.clear()
+#define sz                  size()
+#define F                   first
+#define S                   second
+#define mmst(x,v)           memset(x, v, sizeof(x))
+#define mp(x,y)             make_pair(x,y)
+#define pr                  pair<int, int>
+#define endl                "\n"
+#define PI                  3.14159265358979323846  /* pi */
+#define sqr(x)              (x)*(x)
+#define sign(x)             (x>0)-(x<0)
+#define isPowerOfTwo(n)    (n&& !(n&(n-1)))
+#define all(v)              ((v).begin()),((v).end())
+#define deb(x)              cout << #x << "=" << x << endl
+#define deb2(x, y)          cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define fast_io             ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define epsilon             1e-9
+#define ss                  string
+#define v                   vector<ll>
+#define cs(nn1111)               ss nn1111;cin >> nn1111
+#define cc(nn1111)               char nn1111;cin >> nn1111
+#define ci2(n1111,m1111)            int n1111,m1111;cin >> n1111 >> m1111
+#define ci(nn1111)               int nn1111;cin >> nn1111
+#define cll(nn1111)              ll nn1111;cin >> nn1111
+#define c(nn1111)                cin >> nn1111
+
+using namespace std;
+
+void solve(){
+    ci2(n,m);
+    int a[n];
+    FOR(i,n) a[i]=0;
+    FOR(i,m) {
+        ci2(tmp1,tmp2);
+        tmp1--;tmp2--;
+        a[tmp1]++;
+        a[tmp2]++;
+    }
+    FOR(i,n) cout << a[i] << endl;
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    //c(t);
+    while(t-->0) solve();
+}
---
+++ 478.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+#define mod                 %1000000007
+#define FOR(i,e)            for(int i=0;i<e;i++)
+#define FORE(i,t,e)         for(int i=t;i<e;i++)
+#define pb                  push_back
+#define clr(x)              x.clear()
+#define sz                  size()
+#define F                   first
+#define S                   second
+#define mmst(x,v)           memset(x, v, sizeof(x))
+#define mp(x,y)             make_pair(x,y)
+#define pr                  pair<int, int>
+#define endl                "\n"
+#define PI                  3.14159265358979323846  /* pi */
+#define sqr(x)              (x)*(x)
+#define sign(x)             (x>0)-(x<0)
+#define isPowerOfTwo(n)    (n&& !(n&(n-1)))
+#define all(v)              ((v).begin()),((v).end())
+#define deb(x)              cout << #x << "=" << x << endl
+#define deb2(x, y)          cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define fast_io             ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define epsilon             1e-9
+#define ss                  string
+#define v                   vector<ll>
+#define cs(nn1111)               ss nn1111;cin >> nn1111
+#define cc(nn1111)               char nn1111;cin >> nn1111
+#define ci2(n1111,m1111)            int n1111,m1111;cin >> n1111 >> m1111
+#define ci(nn1111)               int nn1111;cin >> nn1111
+#define cll(nn1111)              ll nn1111;cin >> nn1111
+#define c(nn1111)                cin >> nn1111
+
+using namespace std;
+
+void solve(){
+    ci2(n,m);
+    if(n*(n-1)/2 != m) {
+        cout << "NO" << endl;
+        return;
+    }
+    bool a[n][n];
+    FOR(i,n) FOR(j,n) a[i][j] = false;
+    FOR(i,m) {
+        ci2(t1,t2);
+        t1--;t2--;
+        if(a[t2][t1] || a[t1][t2]) {
+            cout << "NO" << endl;
+            return;
+        }
+        a[t1][t2]=true;
+    }
+    cout << "YES" << endl;
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    //c(t);
+    while(t-->0) solve();
+}
